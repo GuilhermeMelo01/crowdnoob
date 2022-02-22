8ea238ebcc588783aa7af34a8a9a4ee3a3b43ade
GuilhermeMelo01 <Meloguilherme1994@gmail.com>
2022-02-22 13:29:45 -0300

    First Commit

---
+++ src/NotasMoedas.java
@@ -0,0 +1,34 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class NotasMoedas {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scan = new Scanner(System.in);
+
+        float valor;
+        int[] cedulas = {100, 50, 20, 10, 5, 2};
+        int[] moedas = {100, 50, 25, 10, 5, 1};
+        int resto, quociente;
+        valor = scan.nextFloat();
+
+        resto = (int) (valor * 100 + 5);
+
+        System.out.println("NOTAS:");
+        for (double cedula : cedulas) {
+            quociente = (int) (resto / (cedula * 100));
+            System.out.printf(quociente + " nota(s) de R$ %.2f%n", cedula);
+            resto %= (cedula * 100);
+        }
+
+        System.out.println("MOEDAS:");
+        for (double moeda : moedas) {
+            quociente = (int) (resto / moeda);
+            System.out.printf(quociente + " moedas(s) de R$ %.2f%n", moeda / 100);
+            resto %= moeda;
+        }
+        scan.close();
+    }
+
+}
+
